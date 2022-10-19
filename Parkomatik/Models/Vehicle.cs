c6f6f16bf42567b0c5d2847db9cff3c47cc0f2c8
Jaesung Hong <jezsung@gmail.com>
2022-10-19 13:50:18 -0500

    fix missing curly brace

--- Parkomatik/Models/Vehicle.cs
+++ Parkomatik/Models/Vehicle.cs
@@ -5,7 +5,7 @@
         public int ID { get; set; }
         public int PassID { get; set; }
         public virtual Pass Pass { get; set; }
-        public virtual ICollection<Reservation> Reservations { get; set; 
+        public virtual ICollection<Reservation> Reservations { get; set; }
         public bool Parked { get; set; }
     }
 }
