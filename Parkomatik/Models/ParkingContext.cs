114f3d02bc2e508ba502ecde3d1db8dfddda0f17
Jaesung Hong <jezsung@gmail.com>
2022-10-19 13:46:32 -0500

    add provided code

---
+++ Parkomatik/Models/ParkingContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Parkomatik.Models
+{
+    public class ParkingContext : DbContext
+    {
+        public virtual DbSet<Vehicle> Vehicles { get; set; }
+        public virtual DbSet<Pass> Passes { get; set; }
+        public virtual DbSet<ParkingSpot> ParkingSpots { get; set; }
+        public virtual DbSet<Reservation> Reservations { get; set; }
+    }
+}
---
+++ Parkomatik/Models/ParkingHelper.cs
@@ -0,0 +1,35 @@
+﻿namespace Parkomatik.Models
+{
+    public class ParkingHelper
+    {
+        private ParkingContext parkingContext;
+
+        public ParkingHelper(ParkingContext context)
+        {
+            this.parkingContext = context;
+        }
+
+        public Pass CreatePass(string purchaser, bool premium, int capacity)
+        {
+            Pass newPass = new Pass();
+            newPass.Purchaser = purchaser;
+            newPass.Premium = premium;
+            newPass.Capacity = capacity;
+
+            parkingContext.Passes.Add(newPass);
+            parkingContext.SaveChanges();
+
+            return newPass;
+        }
+
+        public ParkingSpot CreateParkingSpot()
+        {
+            ParkingSpot newSpot = new ParkingSpot();
+            newSpot.Occupied = false;
+
+            parkingContext.ParkingSpots.Add(newSpot);
+
+            return newSpot;
+        }
+    }
+}
