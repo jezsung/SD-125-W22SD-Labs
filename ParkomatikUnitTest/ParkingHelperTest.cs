42051906224fc8f95771ea9d3554dea0383d0b53
Jaesung Hong <jezsung@gmail.com>
2022-10-19 19:58:52 -0500

    add ShouldCreateParkingSpot

--- ParkomatikUnitTest/ParkingHelperTest.cs
+++ ParkomatikUnitTest/ParkingHelperTest.cs
@@ -97,5 +97,33 @@ namespace ParkomatikUnitTest
                 parkingHelper.CreatePass("Customer1", true, capacity);
             });
         }
+
+        [TestMethod]
+        public void ShouldCreateParkingSpot()
+        {
+            // Arrange
+            var mockDbContext = new Mock<ParkingContext>();
+
+            var addedParkingSpots = new List<ParkingSpot>();
+            var savedParkingSpots = new List<ParkingSpot>();
+
+            mockDbContext.Setup(x => x.ParkingSpots.Add(It.IsAny<ParkingSpot>())).Callback((ParkingSpot parkingSpot) =>
+            {
+                addedParkingSpots.Add(parkingSpot);
+            });
+            mockDbContext.Setup(x => x.SaveChanges()).Callback(() =>
+            {
+                savedParkingSpots.AddRange(addedParkingSpots);
+                addedParkingSpots.Clear();
+            });
+
+            var parkingHelper = new ParkingHelper(mockDbContext.Object);
+
+            // Act
+            parkingHelper.CreateParkingSpot();
+
+            // Assert
+            Assert.AreEqual(1, savedParkingSpots.Count());            
+        }
     }
 }
