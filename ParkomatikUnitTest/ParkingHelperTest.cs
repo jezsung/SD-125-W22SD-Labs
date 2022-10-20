fdf6165fa6795364aa88532e7b2f2853c00422bd
Jaesung Hong <jezsung@gmail.com>
2022-10-19 19:42:33 -0500

    add ShouldCreatePass to ParkingHelperTest

--- ParkomatikUnitTest/ParkingHelperTest.cs
+++ ParkomatikUnitTest/ParkingHelperTest.cs
@@ -1,4 +1,3 @@
-using Microsoft.EntityFrameworkCore;
 using Moq;
 using Parkomatik.Models;
 
@@ -7,38 +6,37 @@ namespace ParkomatikUnitTest
     [TestClass]
     public class ParkingHelperTest
     {
-        private ParkingContext context;
-
-        [TestInitialize]
-        public void TestInitialize()
-        {
-
-        }
-
         [TestMethod]
         public void ShouldCreatePass()
         {
-            var passes = new List<Pass>();
-
-            var queryablePasses = passes.AsQueryable();
-
-            var mockPassDbSet = new Mock<DbSet<Pass>>();
-            var mockParkingSpotDbSet = new Mock<DbSet<ParkingSpot>>();
-
-            mockPassDbSet.As<IQueryable<Pass>>().Setup(x => x.Provider).Returns(queryablePasses.Provider);
-            mockPassDbSet.As<IQueryable<Pass>>().Setup(x => x.Expression).Returns(queryablePasses.Expression);
-            mockPassDbSet.As<IQueryable<Pass>>().Setup(x => x.ElementType).Returns(queryablePasses.ElementType);
-            mockPassDbSet.As<IQueryable<Pass>>().Setup(x => x.GetEnumerator()).Returns(queryablePasses.GetEnumerator());
-
+            // Arrange
             var mockDbContext = new Mock<ParkingContext>();
 
-            mockDbContext.Setup(x => x.Passes).Returns(mockPassDbSet.Object);
+            var addedPasses = new List<Pass>();
+            var savedPasses = new List<Pass>();
+
+            mockDbContext.Setup(x => x.Passes.Add(It.IsAny<Pass>())).Callback((Pass pass) =>
+            {
+                addedPasses.Add(pass);
+            });
+            mockDbContext.Setup(x => x.SaveChanges()).Callback(() =>
+            {
+                savedPasses.AddRange(addedPasses);
+                addedPasses.Clear();
+            });
 
             var parkingHelper = new ParkingHelper(mockDbContext.Object);
 
+            // Act
             parkingHelper.CreatePass("Customer1", true, 10);
 
-            Assert.AreEqual(1, mockPassDbSet.Object.Count());
-        }
+            // Assert
+            Assert.AreEqual(1, savedPasses.Count());
+
+            var savedPass = savedPasses.First();
+            Assert.AreEqual("Customer1", savedPass.Purchaser);
+            Assert.AreEqual(true, savedPass.Premium);
+            Assert.AreEqual(10, savedPass.Capacity);
+        }       
     }
 }
